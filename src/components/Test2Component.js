edd78a3b63912aaae3f07b6a9cb6a9f609fd6aa4
TATY <tatyccsilva@gmail.com>
2019-07-31 01:17:23 +0200

    Match with target ring

--- src/components/Test2Component.js
+++ src/components/Test2Component.js
@@ -3,28 +3,26 @@ import { TimelineLite, Power0, CSSPlugin, Back } from "gsap";
 import './Test2Component.css'
 
 export default class Test2Component extends Component {
-    // state = {
-    //     play: true
-    // }
-
     tl = new TimelineLite({ paused: true })
-    tl2 = new TimelineLite()
+    tl2 = new TimelineLite({ paused: true })
     object = null
     hoopContainer = null
     hoopTween = null
+    output = null
 
     play = true
 
     componentDidMount() {
-        // tl.from(element, 1, { left: -100 })    //tween element's left from -100
-        //     .to(element, 1, { top: 50 })    //then tween element's "top" to 50
-        //     .set(element, { opacity: 0 })    //then set element's opacity to 0.5 immediately
-        //     .call(otherFunction)    //then call otherFunction()
-        //     .staggerTo([element1, element2, element3], 1.5, { rotation: 45 }, 0.25)
+        const target = this.hoopContainer.getBoundingClientRect() 
+        const object = this.object.getBoundingClientRect()
+        const targetCenterTop = target.top - target.height/2
+        const objectCenterTop = object.top - object.height/2
+        const targetCenterY = targetCenterTop - objectCenterTop
 
         this.tl
             .fromTo(this.object, 1,
                 {
+                    y: 0,
                     x: -200
                 },
                 {
@@ -36,6 +34,18 @@ export default class Test2Component extends Component {
                     yoyo: true
                 })
             .seek(1)
+
+        this.tl2
+            .fromTo(this.object, 1,
+                {
+                    y: 0
+                },
+                {
+                    y: targetCenterY,
+                    rotation: 720,
+                    scaleX: 0.5, scaleY: 0.5,
+                    ease: Back.easeOut.config(3.5)
+                })
     }
 
     play = () => {
@@ -48,31 +58,49 @@ export default class Test2Component extends Component {
         CSSPlugin.useSVGTransformAttr = true;
 
         if (this.play) {
-            this.tl2
-                // .fromTo(this.object, 1,
-                //     {
-                //         y: 0
-                //     },
-                //     {
-                //         y: -400,
-                //         rotation: 720,
-                //     })
-                .to(this.object, 1, {
-                    y: -350,
-                    rotation: 720,
-                    scaleX: 0.5, scaleY: 0.5,
-                    ease: Back.easeOut.config(3.5)
-                })
             this.tl2.play()
             this.tl.pause()
             this.play = false
+
+            const objectWidth = this.object.getBoundingClientRect().width
+            const targetWidth = this.hoopContainer.getBoundingClientRect().width
+            const objectCenter = this.object.getBoundingClientRect().x + objectWidth / 2
+            const targetCenter = this.hoopContainer.getBoundingClientRect().x + targetWidth / 2
+            const ring = targetWidth / (6 * 2)
+
+            console.log('variables', objectWidth, targetWidth, objectCenter, targetCenter, ring)
+
+            if (objectCenter === targetCenter) {
+                return console.log('perfe')
+            }
+            if (objectCenter > targetCenter - ring && objectCenter < targetCenter + ring) {
+                return console.log('primero')
+            }
+            if (objectCenter > targetCenter - (2 * ring) && objectCenter < targetCenter + (2 * ring)) {
+                return console.log('segundo')
+            }
+            if (objectCenter > targetCenter - (3 * ring) && objectCenter < targetCenter + (3 * ring)) {
+                return console.log('tercero')
+            }
+            if (objectCenter > targetCenter - (4 * ring) && objectCenter < targetCenter + (4 * ring)) {
+                return console.log('quarto')
+            }
+            if (objectCenter > targetCenter - (5 * ring) && objectCenter < targetCenter + (5 * ring)) {
+                return console.log('quinto')
+            }
+            if (objectCenter > targetCenter - (6 * ring) && objectCenter < targetCenter + (6 * ring)) {
+                return console.log('sexto')
+            } 
+            else {
+                return console.log('fuera!')
+            }
         }
     }
 
     tryAgain = () => {
         if (!this.play) {
-            this.tl.restart()
             this.tl2.reverse()
+            this.tl.restart()
             this.play = true
         }
     }
@@ -101,13 +129,23 @@ export default class Test2Component extends Component {
                     </div>
                 </div>
                 <div className="botton">
-                    <img
+                    <div
+                        className="basket-object"
+                        ref={img => this.object = img}
+                        onClick={this.throw}
+                    >
+                        +
+                    </div>
+                    {/* <img
                         src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/logo-man.svg"
                         alt=""
                         className="basket-object"
                         ref={img => this.object = img}
                         onClick={this.throw}
-                    />
+                    /> */}
+                </div>
+                <div>
+                    <p ref={p => this.output = p}></p>
                 </div>
             </div>
         )
